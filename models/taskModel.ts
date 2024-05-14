d264ddec04c8556b052452ced18a1840f49ecd35
ldphat99 <ldphat99@gmail.com>
2024-05-14 16:58:24 +0700

    feat: define schema and connect to database
    Define a schema for Task
    
    Connect to MongoDB

---
+++ models/taskModel.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+import { ObjectId } from "https://deno.land/x/mongo@v0.32.0/deps.ts";
+
+interface TaskSchema {
+    _id: ObjectId;
+    taskName: string;
+    taskCompleted: boolean;
+}
+
+const client = new MongoClient();
+await client.connect("mongodb://127.0.0.1:27017");
+const db = client.database("hono_todolist");
+const Tasks = db.collection<TaskSchema>("task");
+
+export default Tasks;
