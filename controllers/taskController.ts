05e804770ecbb38193aecea97881c21158a856f7
ldphat99 <ldphat99@gmail.com>
2024-05-16 08:11:34 +0700

    refactor(controller): refactor taskController to taskService

--- controllers/taskController.ts
+++ controllers/taskController.ts
@@ -4,117 +4,105 @@ import { Bson } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 import { CreateTaskReq, UpdateTaskReq } from "../types/type.ts";
 import Tasks from "../models/taskModel.ts";
 
+import { getAllTasks, getTaskById, createTaskService, deleteTaskById, deleteAllTasks, updateTaskById } from "../services/taskService.ts";
+
 //* Get all tasks
 export const getTasks = async (c: Context) => {
-    const allTasks = await Tasks.find({ taskName: undefined }).toArray();
-
-    return c.json({ data: allTasks });
+    try {
+        const allTasks = await getAllTasks();
+        return c.json(allTasks);
+    } catch (error) {
+        c.status(500);
+        return c.json({ message: error.message });
+    }
 }
 
 //* Get a task
 export const getTask = async (c: Context) => {
-    const taskId = c.req.param('id') as string;
-
-    if (!Bson.ObjectId.isValid(taskId)) {
-        c.status(400);
-        return c.json({
-            message: 'Invalid task ID',
-        });
+    try {
+        const taskId = c.req.param("id") as string;
+        const task = await getTaskById(taskId);
+        return c.json(task);
+    } catch (error) {
+        if (error.message === "Invalid task ID") {
+            c.status(400);
+        } else if (error.message === "Task not found") {
+            c.status(404);
+        } else {
+            c.status(500);
+        }
+        return c.json({ message: error.message });
     }
-
-    const task = await Tasks.findOne({ _id: new Bson.ObjectId(taskId) });
-
-    if (!task) {
-        return c.json({ message: "Task not found", });
-    }
-
-    return c.json({ data: task, });
 }
 
 //* Create a new task
 export const createTask = async (c: Context) => {
-    const { taskName }: CreateTaskReq = await c.req.json();
-
-    const taskExists = await Tasks.findOne({ taskName });
-
-    if (taskExists) {
-        c.status(409);
-        throw new Error('Task already exists');
-    };
-
-    const task = await Tasks.insertOne({
-        taskName,
-        taskCompleted: false,
-    });
-
-    if (!task) {
-        c.status(400);
-        throw new Error('Invalid task data');
-    };
-
-    const retrieveTask = await Tasks.findOne({ _id: task });
-
-    return c.json({
-        data: retrieveTask,
-        message: 'Task created successfully',
-    });
+    try {
+        const taskData = await c.req.json();
+        const newTask = await createTaskService(taskData);
+        return c.json(newTask);
+    } catch (error) {
+        if (error.message === "Task already exists") {
+            c.status(409);
+        } else if (error.message === "Invalid task data") {
+            c.status(400);
+        } else {
+            c.status(500);
+        }
+        return c.json({ message: error.message });
+    }
 }
 
 //* Update a task
 export const updateTask = async (c: Context) => {
-    const taskId = c.req.param('id') as string;
-    const { taskName, taskCompleted }: UpdateTaskReq = await c.req.json();
+    try {
+        const taskId = c.req.param("id") as string;
+        const payload: UpdateTaskReq = await c.req.json();
+        const result = await updateTaskById(taskId, payload);
+        
+        return c.json(result);
+    } catch (error) {
+        if(error.message === 'Invalid task ID') {
+            c.status(400);
+        } else if (error.message === 'Task not found') {
+            c.status(404);
+        } else {
+            c.status(500);
+        }
 
-    if (!Bson.ObjectId.isValid(taskId)) {
-        c.status(400);
-        return c.json({
-            message: 'Invalid task ID',
-        });
+        return c.json({ message: error.message });
     }
-
-    const filter = { _id: new Bson.ObjectId(taskId) };
-    const update = { $set: { taskName, taskCompleted } };
-
-    const result = await Tasks.updateOne(filter, update);
-
-    if (result.matchedCount === 0) {
-        c.status(404);
-        return c.json({ message: 'Task not found' });
-    }
-
-    return c.json({
-        message: 'Task updated successfully',
-    });
 }
 
 //* Delete all tasks
 export const deleteTasks = async (c: Context) => {
-    const deleteCount = await Tasks.deleteMany({ });
-
-    if (deleteCount === 0) {
-        return c.json({ message: 'No tasks found to delete' });
+    try {
+        const deletedCount = await deleteAllTasks();
+        return c.json({ message: `${deletedCount} Tasks deleted successfully` });
+    } catch (error) {
+        if (error.message === "No tasks found to delete") {
+            c.status(404);
+        } else {
+            c.status(500);
+        }
+        return c.json({ message: error.message });
     }
-
-    return c.json({
-        message: `${deleteCount} Tasks deleted successfully`,
-    });
 }
 
 //* Delete a task
 export const deleteTask = async (c: Context) => {
-    const taskId = c.req.param("id");
-
-    if (!Bson.ObjectId.isValid(taskId)) {
-        return c.json({ message: 'Invalid task ID' });
+    try {
+        const taskId = c.req.param("id") as string;
+        const deletedCount = await deleteTaskById(taskId);
+        return c.json({ message: "Task deleted successfully" });
+    } catch (error) {
+        if (error.message === "Invalid task ID") {
+            c.status(400);
+        } else if (error.message === "Task not found") {
+            c.status(404);
+        } else {
+            c.status(500);
+        }
+        return c.json({ message: error.message });
     }
-
-    const deleteCount = await Tasks.deleteOne({ _id: new Bson.ObjectId(taskId) });
-
-    if (deleteCount === 0) {
-        return c.json({ message: 'Task not found' });
-    }
-
-    return c.json({
-        message: 'Task deleted successfully',
-    });
 }
---
+++ services/taskService.ts
@@ -0,0 +1,82 @@
+import { Bson } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+import { CreateTaskReq, UpdateTaskReq } from "../types/type.ts";
+
+import Tasks from "../models/taskModel.ts";
+
+export const getAllTasks = async () => {
+    return await Tasks.find({ taskName: undefined }).toArray();
+};
+
+export const getTaskById = async (taskId: string) => {
+    if (!Bson.ObjectId.isValid(taskId)) {
+        throw new Error("Invalid task ID");
+    }
+
+    const task = await Tasks.findOne({ _id: new Bson.ObjectId(taskId) });
+    if (!task) {
+        throw new Error("Task not found");
+    }
+
+    return task;
+};
+
+export const createTaskService = async (taskData: CreateTaskReq) => {
+    const { taskName } = taskData;
+    const taskExists = await Tasks.findOne({ taskName });
+    
+    if (taskExists) {
+        throw new Error("Task already exists");
+    }
+
+    const task = await Tasks.insertOne({ taskName, taskCompleted: false });
+    if (!task) {
+        throw new Error("Invalid task data");
+    }
+
+    return await Tasks.findOne({ _id: task });
+};
+
+export const updateTaskById = async (taskId: string, taskData: UpdateTaskReq) => {
+    if (!Bson.ObjectId.isValid(taskId)) {
+        throw new Error("Invalid task ID");
+    }
+
+    const { taskName, taskCompleted }: UpdateTaskReq = taskData;
+
+    const filter = { _id: new Bson.ObjectId(taskId) };
+    const update = { $set: { taskName, taskCompleted } };
+
+    const result = await Tasks.updateOne(filter, update);
+
+    if (result.matchedCount === 0) {
+        throw new Error('Task not found');
+    }
+
+    return getTaskById(taskId);
+};
+
+export const deleteAllTasks = async () => {
+    const deleteCount = await Tasks.deleteMany({});
+
+    if (deleteCount === 0) {
+        // throw new Error('No tasks found to delete');
+    }
+
+    return deleteCount;
+}
+
+export const deleteTaskById = async (taskId: string) => {
+    if (!Bson.ObjectId.isValid(taskId)) {
+        throw new Error("Invalid task ID");
+    }
+
+    const deleteCount = await Tasks.deleteOne({ _id: new Bson.ObjectId(taskId) });
+
+    if (deleteCount === 0) {
+        throw new Error('Task not found');
+    }
+
+    return deleteCount;
+}
+
+
