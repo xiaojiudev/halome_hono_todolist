0765b559b9bd240d42fbf8af63c2864aed236019
ldphat99 <ldphat99@gmail.com>
2024-05-18 10:57:06 +0700

    refactor: add connect to database function, restructure for setup and routing
    restructure for server setup and routing by splitting into functions

--- main.ts
+++ main.ts
@@ -1,27 +1,40 @@
-import { Hono } from 'https://deno.land/x/hono@v4.3.6/mod.ts'
-import { cors } from 'https://deno.land/x/hono@v4.3.6/middleware.ts'
+import { Hono } from 'https://deno.land/x/hono@v4.3.6/mod.ts';
+import { cors } from 'https://deno.land/x/hono@v4.3.6/middleware.ts';
 
-import tasks from "./routers/taskRoute.ts";
+import { connectDB } from "./models/db.ts";
+import taskRoute from "./routers/taskRoute.ts";
+import { APP_PORT } from "./utils/bootstrap.ts";
 
 const app = new Hono();
 
-function startServer(): void {
-    try {
-        app.use(
-            '*',
-            cors({
-                origin: '*',
-                allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-            })
-        );
-        
-        //* Task Route
-        app.route('/tasks', tasks);
+startServer();
 
-        Deno.serve({ port: 8080 }, app.fetch);
+async function startServer(): Promise<void> {
+    try {
+        await connectDB();
+
+        appConfig();
+
+        appRoutes();
+
+        Deno.serve({ port: APP_PORT }, app.fetch);
     } catch (error) {
         console.log("Start server failed!", error);
     }
 }
 
-startServer();
+//* Setup app configuration
+function appConfig(): void {
+    app.use(
+        '*',
+        cors({
+            origin: '*',
+            allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        })
+    );
+}
+
+//* Define app routes
+function appRoutes(): void {
+    app.route('/tasks', taskRoute);
+}
