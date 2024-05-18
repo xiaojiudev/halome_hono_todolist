a6691f2767516a6c49bd4fb1f1cdfb020f2a0af1
ldphat99 <ldphat99@gmail.com>
2024-05-18 10:48:04 +0700

    refactor: optimize connection to mongdb

--- models/taskModel.ts
+++ models/taskModel.ts
@@ -1,15 +1,19 @@
-import { MongoClient } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 import { ObjectId } from "https://deno.land/x/mongo@v0.32.0/deps.ts";
+import { Collection } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
+import { getDb } from "./db.ts";
 export interface TaskSchema {
     _id: ObjectId;
     taskName: string;
     taskCompleted: boolean;
 }
 
-const client = new MongoClient();
-await client.connect("mongodb://127.0.0.1:27017");
-const db = client.database("hono_todolist");
-const Tasks = db.collection<TaskSchema>("task");
+let tasksCollection: Collection<TaskSchema> | null = null;
 
-export default Tasks;
+export const getTasksCollection = async () => {
+    if (tasksCollection) return tasksCollection;
+
+    const db = await getDb();
+    tasksCollection = db.collection<TaskSchema>("task");
+    return tasksCollection;
+};
